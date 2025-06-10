e7393958bcfc8bed6b19153af70d795e9e356a54
2sandeep7 <ssandeep8696@gmail.com>
2025-06-11 00:19:46 +0530

    fix: small UI fix

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ function Landing() {
   }, [])
 
   return (
-    <div className="min-h-screen bg-background antialiased max-w-5xl mx-auto py-10 sm:py-6 px-6 transition-all bg-[#001100] font-switzer">
+    <div className="min-h-screen  antialiased max-w-5xl mx-auto py-10 sm:py-6 px-6 transition-all  font-switzer">
       <div className="flex flex-col gap-20">
           <Suspense fallback={<div>
             <div className="flex justify-center items-center h-screen">
